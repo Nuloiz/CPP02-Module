55724dd3f59b898aab12317ae04f6c36d59ed911
nschutz <nschutz@student.42wolfsburg.de>
2024-02-20 14:42:34 +0100

    CPP02 feat: ex01 finished

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -11,7 +11,7 @@ Fixed::Fixed(const Fixed &other) {
 
 Fixed & Fixed::operator=(const Fixed &other) {
     std::cout << "Copy assignment operator called" << std::endl;
-    value = other.getRawBits();
+    value = other.value;
     return (*this);
 }
 
